83db2b1376254b3bde7cb0834aeb90bb0f15f7d0
Andoni Mendoza <andoni.mendoza@gmail.com>
2017-10-05 16:09:17 -0500

    added readme

---
+++ servicely-ui-mockup/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  servicely-ui-mockup
+//
+//  Created by Andoni Mendoza on 10/5/17.
+//  Copyright © 2017 Andoni Mendoza. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
